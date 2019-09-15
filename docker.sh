ccfd44533400a7fbc1ebfe0bfc81a7586c8fcbf9
Ankit Pati <contact@ankitpati.in>
2019-09-15 14:57:43 +0530

    prepare to use `sudo` instead of `su`

--- docker.sh
+++ docker.sh
@@ -3,10 +3,8 @@
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
-test "$#" -ne 0 && args=(-c "$*")
-
 docker build --tag='mojodev' . && \
 docker run --mount type=bind,src="$(pwd)",dst='/opt/mojo' \
     --publish 3000:3000 \
     --publish 8080:8080 \
-    -it mojodev "${args[@]}"
+    -it mojodev "$@"
