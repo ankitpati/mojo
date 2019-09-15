5a05af6dcc0af56c2f228b8b1173982c2553a369
Ankit Pati <contact@ankitpati.in>
2019-09-15 19:41:50 +0530

    confirm to the old interface through docker.sh

--- docker.sh
+++ docker.sh
@@ -3,8 +3,10 @@
 mydir="$(dirname "$0")/"
 cd "$mydir"
 
+test "$#" -ne 0 && args=(bash -lc "$*")
+
 docker build --tag='mojodev' . && \
 docker run --mount type=bind,src="$(pwd)",dst='/opt/mojo' \
     --publish 3000:3000 \
     --publish 8080:8080 \
-    -it mojodev "$@"
+    -it mojodev "${args[@]}"
