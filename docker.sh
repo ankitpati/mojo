02f9db70b7e23c1abf85fbfbe44f7e8bc503bc9c
Ankit Pati <contact@ankitpati.in>
2019-09-18 14:57:20 +0530

    don’t overwrite the fedora mojodev

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ cd "$mydir"
 
 test "$#" -ne 0 && args=(bash -lc "$*")
 
-docker build --tag='mojodev' . && \
+docker build --tag='mojodev-ubuntu' . && \
 docker run --mount type=bind,src="$(pwd)",dst='/opt/mojo' \
     --publish 3000:3000 \
     --publish 8080:8080 \
